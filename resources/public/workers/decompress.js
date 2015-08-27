85585aae68ce7ec31128ae9db3a75ea426b7f541
Uday Verma <uday.karan@gmail.com>
2015-08-27 14:33:24 -0500

    Configuration changes, update renderer/lib stuff

--- resources/public/workers/decompress.js
+++ resources/public/workers/decompress.js
@@ -28,8 +28,53 @@ function swapSpace(buffer, worldBoundsX, pointSize, numPoints) {
 	}
 }
 
+var hasColor = function(schema) {
+	var hasr = false, hasg = false, hasb = false;
+	for (var i = 0, il = schema.length ; i < il ; i ++) {
+		hasr = hasr || schema[i].name === "Red";
+		hasg = hasg || schema[i].name === "Green";
+		hasb = hasb || schema[i].name === "Blue";
+	}
+
+	return hasr && hasb && hasg;
+};
+
+var addColor = function(input, numPoints, pointSize) {
+	// if we're adding color the point size is going to be 3 floats more
+	//
+	var newPointSize = pointSize + 12;
+	var newBuffer = new Float32Array(numPoints * newPointSize / 4);
+
+	var off = 0,
+		woff = 0;
+	var leftOver = pointSize - 12,
+		instride = pointSize / 4,
+		outstride = newPointSize / 4;
+
+	for (var i = 0 ; i < numPoints ; i ++) {
+		newBuffer[woff + 0] = input[off + 0];
+		newBuffer[woff + 1] = input[off + 1];
+		newBuffer[woff + 2] = input[off + 2];
+
+		// color
+		newBuffer[woff + 3] = 0;
+		newBuffer[woff + 4] = 0;
+		newBuffer[woff + 5] = 0;
+
+		// whatever else
+		for (var j = 0, jl = leftOver / 4 ; j < jl ; j ++) {
+			newBuffer[woff + 6 + j] = input[off + 3 + j];
+		}
+
+		off += instride;
+		woff += outstride;
+	}
+
+	return newBuffer;
+};
+
 var totalSaved = 0;
-var decompressBuffer = function(schema, worldBoundsX, ab, numPoints) {
+var decompressBuffer = function(schema, worldBoundsX, ab, numPoints, checkAddColor) {
 	var x = new Module.DynamicLASZip();
 
 	var abInt = new Uint8Array(ab);
@@ -70,6 +115,12 @@ var decompressBuffer = function(schema, worldBoundsX, ab, numPoints) {
 
 	var b = new Float32Array(ret.buffer);
 
+	if (checkAddColor && numPoints > 0 && !hasColor(schema)) {
+		console.log(b.byteLength);
+		b = addColor(b, numPoints, pointSize);
+		console.log(b.byteLength);
+	}
+
 	// if we got any points, swap them
 	if (numPoints > 0)
 		swapSpace(b, worldBoundsX, pointSize, numPoints);
@@ -84,7 +135,8 @@ self.onmessage = function(e) {
 	var ab = data.buffer;
 	var numPoints = data.pointsCount;
 	var worldBoundsX = data.worldBoundsX;
+	var addColor = data.addColor;
 
-	var res = decompressBuffer(schema, worldBoundsX, ab, numPoints);
+	var res = decompressBuffer(schema, worldBoundsX, ab, numPoints, addColor);
 	postMessage({result: res}, [res.buffer]);
 };
