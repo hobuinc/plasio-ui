174773224bfb19364eaf202b46bcdfa3e5b6d5b8
Uday Verma <uday.karan@gmail.com>
2017-12-04 17:28:50 -0600

    Externs to go with timeline widget

--- src/vendor/externs/extras.js
+++ src/vendor/externs/extras.js
@@ -3,3 +3,10 @@
 
 vec3 = {};
 vec3.angle = function(a, b) {};
+
+
+vis = {}
+vis.Timeline = function() {};
+vis.Timeline.prototype.addCustomTime = function(id) {};
+vis.Timeline.prototype.on = function(event, f) {};
+vis.Timeline.prototype.setCustomTime = function(time, id) {};
